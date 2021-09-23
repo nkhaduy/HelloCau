de206c46e52b2db26bda540a3d5a27e6f77adcbf
nkhaduy <nkhaduy@gmail.com>
2021-09-23 20:55:02 +0700

    abc

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Helooooooo <3 🖐🖐🖐',
+    introDesc: `Tui có điều này muốn hỏi bạn :)), Bạn nhớ phải trả lời thật lòng nhaaa!!`,
+    btnIntro: 'Okeee',
+    title: 'Bạn thích tui đúng hemmmmmmm 😙',
+    desc: 'Bạn không trả lời hoặc thoát ra, tức là bạn thích tui đó nha :> ',
+    btnYes: 'Đúng rùi, tui thích bạn ó 💖💖💖',
+    btnNo: '💔 Mơ hả đĩ 💔 ',
+    question:'Nói cho tui biết đi vì sao bạn thích tui 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'Tại vì bạn đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Tui biết mà, tui cũng thích bạn lắm 🥰. Love you 3.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tui nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/nkhaduyyy'
+}
